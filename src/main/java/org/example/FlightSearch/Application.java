d5dc2b56066f46372eef80c04127f9abd5c9b242
marika9393 <marika-piotrowska@wp.pl>
2020-10-26 14:05:49 +0100

    Initial commit

---
+++ src/main/java/org/example/FlightSearch/Application.java
@@ -0,0 +1,11 @@
+package org.example.FlightSearch;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        Flight flight = new Flight("Warsaw", "Rome");
+
+        flight.flightFromTo();
+    }
+}
